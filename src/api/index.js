be7c6e13fbb28bb28e9935935847f59aa3fca678
Bohdan Khomych <bod.khomych@gmail.com>
2023-01-15 12:22:08 +0200

    upd

--- src/api/index.js
+++ src/api/index.js
@@ -1,14 +1,18 @@
-const MAIN_KEY = "items";
-function saveKey (data){
-    const mainArray = arrayCheck();
-     mainArray.push(data);
- localStorage.setItem(MAIN_KEY,JSON.stringify(mainArray));
- };
- function arrayCheck(){
-    try{
+const MAIN_KEY = 'items';
+function saveKey(data) {
+  const mainArray = arrayCheck();
+  mainArray.push(data);
+  addDataToStorage(mainArray);
+}
+function arrayCheck() {
+  try {
     const tempKey = localStorage.getItem(MAIN_KEY);
-    return tempKey ? JSON.parse (tempKey): [];
-    }
-    catch(error){console.log(error.message)}
-};
-export {saveKey,arrayCheck};
+    return tempKey ? JSON.parse(tempKey) : [];
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+function addDataToStorage(arr) {
+  localStorage.setItem(MAIN_KEY, JSON.stringify(arr));
+}
+export { saveKey, arrayCheck, addDataToStorage };
--- src/main.js
+++ src/main.js
@@ -1,27 +1,74 @@
-import {saveKey,arrayCheck} from "./api";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./css/style.css";
-const submitL = document.querySelector('.form')
-function onSubmit (evt){
-    evt.preventDefault();
-console.dir(evt.target.elements.message.value)
-const valueI = evt.target.elements.message.value.trim();
-if(!valueI ){
-    return
+import { saveKey, arrayCheck, addDataToStorage } from './api';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/style.css';
+const submitL = document.querySelector('.form');
+const listRef = document.querySelector('.list');
+
+init();
+
+listRef.addEventListener('click', onButtonClick);
+
+function onSubmit(evt) {
+  evt.preventDefault();
+  console.dir(evt.target.elements.message.value);
+  const valueI = evt.target.elements.message.value.trim();
+  if (!valueI) {
+    return;
+  }
+  const obj = objFabric(valueI);
+  saveKey(obj);
+  console.log(1, 2, 3);
+  evt.target.reset();
+
+  const markup = createMarkup([obj]);
+  addMarkup(markup);
 }
-const obj = objFabric(valueI);
-saveKey(obj);
-console.log(1,2,3)
-evt.target.reset()
-}
-function objFabric(value){
-    return {value,cheked:false,id:Date.now(),}
+function objFabric(value) {
+  return { value, cheked: false, id: Date.now() };
 }
 // console.log(arrayCheck());
-submitL.addEventListener('submit', onSubmit )
+submitL.addEventListener('submit', onSubmit);
+function init() {
+  const arr = arrayCheck();
 
+  if (!arr.length) {
+    return;
+  }
 
+  const markup = createMarkup(arr);
+  console.log(markup);
 
+  addMarkup(markup);
+}
 
+function createMarkup(arr = []) {
+  return arr
+    .map(item => {
+      return `<li class="item ${item.checked ? 'checked' : ''}" data-id="${item.id}">
+          <p class="text">${item.value}</p>
+          <button type="button" class="button">x</button>
+        </li>`;
+    })
+    .join('');
+}
 
+function addMarkup(markup) {
+  listRef.insertAdjacentHTML('beforeend', markup);
+}
 
+function onButtonClick(evt) {
+  if (evt.target.nodeName !== 'BUTTON') {
+    return;
+  }
+
+  const item = evt.target.closest('.item');
+  const id = item.dataset.id;
+  console.log(id);
+
+  const data = arrayCheck();
+  const filteredArr = data.filter(item => item.id.toString() !== id.toString());
+
+  console.log(filteredArr);
+  addDataToStorage(filteredArr);
+  item.remove();
+}
