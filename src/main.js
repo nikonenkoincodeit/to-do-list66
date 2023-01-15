9418cbc8fe2eb722b946b57c7def0819388116d6
DmytroVyhovskyi1997 <111079087+DmytroVyhovskyi1997@users.noreply.github.com>
2023-01-15 10:45:48 +0200

    commit

--- src/main.js
+++ src/main.js
@@ -1,2 +1,24 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/style.css";
+  
+
+const submitL = document.querySelector('.form')
+
+function onSubmit (evt){
+    evt.preventDefault();
+console.dir(evt.target.elements.message.value)
+const valueI = evt.target.elements.message.value.trim();
+if(!valueI ){
+    return
+}
+console.log(1,2,3)
+evt.target.reset()
+}
+
+submitL.addEventListener('submit', onSubmit )
+
+
+
+
+
+
